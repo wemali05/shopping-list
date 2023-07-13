5ca3a4d746688d28371196dd1963b8172f1b1198
Alfred Wemali <alfredwemali05@gmail.com>
2023-07-13 21:14:34 +0300

    list items

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+      ),
+      body: ListView.builder(
+          itemBuilder: (ctx, index) => ListTile(
+                title: Text(groceryItems[index].name),
+                leading: Container(
+                  height: 24,
+                  width: 24,
+                  color: groceryItems[index].category.color,
+                ),
+                trailing: Text(groceryItems[index].quantity.toString()),
+              )),
+    );
+  }
+}
