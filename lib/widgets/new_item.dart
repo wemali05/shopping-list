d40b8cac2276103dff6fbfbc26e043ec420e8bac
Alfred Wemali <alfredwemali05@gmail.com>
2023-07-21 08:17:04 +0300

    send post request with http

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,13 +23,29 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectecCategory),
+      final url = Uri.https('flutter-prep-e946b-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectecCategory.name,
+          },
+        ),
       );
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //       id: DateTime.now().toString(),
+      //       name: _enteredName,
+      //       quantity: _enteredQuantity,
+      //       category: _selectecCategory),
+      // );
     }
   }
 
